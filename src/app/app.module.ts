03d6139b9493a89c4163b912e198dc3030889a26
gwfernandez <gwfernandez@gmail.com>
2020-06-28 15:45:56 -0300

    create main project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './shared/error/not-found/not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    NavbarComponent,
+    SidebarComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
